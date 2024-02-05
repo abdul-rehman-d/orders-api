dfd07bd22ba7baee7d6e9ec9ae159087ced9e76e
Abdul Rehman <ar618211@gmail.com>
2024-02-06 03:24:09 +0500

    chi logger middleware

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 
 	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
 
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, world!"))
